a12733aeec64637cfc4f5295ba341e6903407378
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2020-06-28 22:33:01 +0300

    added progress and password flipping

--- src/js/SingUp.js
+++ src/js/SingUp.js
@@ -3,14 +3,40 @@
   const signupPwdInput = document.getElementById('password-sign');
   const signupPwdShow = document.getElementById('password-sign-show');
 
+  const signupConfPwdInput = document.getElementById('password-confirm-sign');
+  const signupConfPwdShow = document.getElementById('password-confirm-sign-show');
 
-  signupPwdShow.addEventListener('mouseenter',
-      () => {
-        signupPwdInput.setAttribute('type', 'text');
-      });
-  signupPwdShow.addEventListener('mouseleave',
-      () => {
-        signupPwdInput.setAttribute('type', 'password');
-      });
+
+  addMouseEnterShow(signupPwdInput, signupPwdShow);
+  addMouseLeaveShow(signupPwdInput, signupPwdShow);
+
+  addMouseEnterShow(signupConfPwdInput, signupConfPwdShow);
+  addMouseLeaveShow(signupConfPwdInput, signupConfPwdShow);
+
+  /**
+   * Add mouseenter event listener for text type
+   * @param {HtmlInputElement} pwdInput
+   * @param {HtmlImageElement} pwdShowImg
+   */
+  function addMouseEnterShow(pwdInput, pwdShowImg) {
+    pwdShowImg.addEventListener('mouseenter',
+        () => {
+          pwdInput.setAttribute('type', 'text');
+        },
+    );
+  }
+  /**
+   * Add mouseleave event listener for password type
+   * @param {HtmlpwdInput} pwdInput
+   * @param {HtmlImageElement} pwdShowImg
+   */
+  function addMouseLeaveShow(pwdInput, pwdShowImg) {
+    pwdShowImg.addEventListener('mouseleave',
+        () => {
+          pwdInput.setAttribute('type', 'password');
+        },
+    );
+  }
+
 
 })();
